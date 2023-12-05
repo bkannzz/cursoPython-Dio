50c190343e53a1b4af4660741ba667c91fc6f7fc
rebecca <rebeccagamer7@gmail.com>
2023-12-05 18:21:30 -0300

    atualizando

---
+++ aulas-python/variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Rebecca"
+idade = 15
+
+nome, idade = "Rebecca", 15
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
