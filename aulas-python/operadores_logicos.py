623dd587cf79e76db2ccf3cada25a68f1f6b7a69
rebecca <rebeccagamer7@gmail.com>
2023-12-06 00:57:36 -0300

    att

--- aulas-python/operadores_logicos.py
+++ aulas-python/operadores_logicos.py
@@ -2,13 +2,19 @@
 # OR = para ser True apenas um tem que ser True
 
 
+print(True and True)
+print(True and False)
+print(False and False)
+print(True or True)
+print(True or False)
+print(True or False)
 
 saldo = 1000
 saque = 250
 limite = 200
 conta_especial = True 
 
-exp = saldo >= saque and saque <= limite and saldo >= saque
+exp = saldo >= saque and saque <= limite or conta_especial and saldo >= saque
 print(exp)
 
 exp_2 = (saldo >= saque and saque <= limite) or (conta_especial and saldo >= saque)
