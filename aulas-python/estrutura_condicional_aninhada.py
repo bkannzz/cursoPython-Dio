851856d51ea6236c7daa1879b3467b554a71fdd1
rebecca <rebeccagamer7@gmail.com>
2023-12-07 04:34:16 -0300

    ATT

---
+++ aulas-python/estrutura_condicional_aninhada.py
@@ -0,0 +1,20 @@
+conta_normal = False
+conta_universitaria = True
+
+saldo = 0
+saque = 500
+cheque_especial = 450
+
+if conta_normal:
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    elif saque <= (saldo + cheque_especial):
+        print("Saque realizado com uso do cheque especial!")
+    else:
+        print("Não foi possivel realizar o saque, saldo insuficiente!")
+
+elif conta_universitaria:
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    else:
+        print("Saldo insuficiente!")
--- aulas-python/estruturas_condicionais.py
+++ aulas-python/estruturas_condicionais.py
@@ -1,9 +1,16 @@
 MAIOR_IDADE = 18
+IDADE_ESPECIAL = 12
 
 idade = int(input("Informe sua idade: "))
 
-if idade > 18:
+if idade >= 18:
     print("Maior de idade, pode tirar a CNH.")
+else:
+    print("Ainda não pode tirar a CNH.")
 
-if idade < MAIOR_IDADE:
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH.")
+elif idade == IDADE_ESPECIAL:
+    print("Pode fazer aulas teóricas, mas não pode fazer aulas práticas.")
+else:
     print("Ainda não pode tirar a CNH.")
