658e3d36250d50f552c1fbd9b6250260c6731a5c
rebecca <rebeccagamer7@gmail.com>
2023-12-10 04:39:10 -0300

    att

--- desafios-python/desafio01.py
+++ desafios-python/desafio01.py
@@ -34,11 +34,31 @@ while True:
         
         excedeu_limite = valor > limite
 
-        excedeu_saques + numero_saques >= LIMITE_SAQUES
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedidos.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
 
     elif opcao == "e":
-        print("Extrato")
-
+        print("\n============== EXTRATO ==============")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("=======================================")
+        
     elif opcao == "q":
         break
 
