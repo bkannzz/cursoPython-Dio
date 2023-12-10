b65114ddc219b9e6042159fcca97e609ea04c475
rebecca <rebeccagamer7@gmail.com>
2023-12-10 03:09:13 -0300

    att

---
+++ aulas-python/string_4.py
@@ -0,0 +1,12 @@
+nome = "Rebecca"
+
+mensagem = f"""
+    Olá meu nome é {nome}, 
+  Eu estou aprendendo python.
+      Essa mensagem tem diferentes recuos.
+"""
+
+print(mensagem)
+
+
+print("rebecca lobato comeu \na roupa do rei de roma")
